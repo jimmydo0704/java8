3c152e1a779c03870c0133c1907a1be2181a4a5a
Dat Do <dvdat@tma.com.vn>
2018-08-31 10:37:55 +0700

    TODO list for generics

---
+++ generics/src/test/java/com/dvdat/generics/Main.java
@@ -0,0 +1,35 @@
+package com.dvdat.generics;
+
+public class Main {
+    // TODO
+    // Interface
+
+    // Class
+
+    // Method
+
+    // Type inference
+
+    // Wildcard
+    //  - Upper
+    //  - Lower
+    //  - Unbounded
+
+    // Bounded type
+    //  - Single
+    //  - Multiple
+
+    // Substitution
+
+    // Covariant
+
+    // Contravariant
+
+    // Raw type
+
+    // Reflection
+
+    // Factional interface
+
+    // Intersection type
+}
